c1a98349977aac6af4a1d46f82ae5aa21bb8e563
Maciek Kasprzyk <mkasprzyk@szy.fr>
2016-02-10 22:04:47 +0100

    Add another handler

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from handlers.base import BaseHandler
+from handlers.computers import ComputersHandler
 
 base_urls = [
-    (r"/", BaseHandler)]
+    (r"/", BaseHandler),
+    (r"/computers", ComputersHandler),]
