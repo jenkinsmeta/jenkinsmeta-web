79ee951018566790a6bcffe958b77ee3fada8074
Maciej Kasprzyk <mkasprzyk@szy.fr>
2016-02-16 11:04:55 -0500

    Testing template

--- handlers/base.py
+++ handlers/base.py
@@ -5,8 +5,15 @@ class BaseHandler(tornado.web.RequestHandler):
     def get(self):
         content = {
                 'message': 'Hello from Tornado!', 
+                'jobs': [
+                    {'name': 'Example1', 'url': 'http://some.jenkins/job/Example1'},
+                    {'name': 'Example2', 'url': 'http://some.jenkins/job/Example2'},
+                    {'name': 'Example3', 'url': 'http://some.jenkins/job/Example3'},
+                    {'name': 'Example4', 'url': 'http://some.jenkins/job/Example4'},
+                    {'name': 'Example5', 'url': 'http://some.jenkins/job/Example5'}
+                ]
         }
-        self.render("base.html", **content)
+        self.render("index.html", **content)
 
     def post(self):
         self.write(self.request.body)
